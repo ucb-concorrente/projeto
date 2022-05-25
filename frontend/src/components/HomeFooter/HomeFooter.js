5dd692772d403cd8e6fe4305d45d61d65b66bfb3
Arthur <arthur.carneirontk@gmail.com>
2022-05-25 20:23:14 -0300

    Fix: Home Footer

--- frontend/src/components/HomeFooter/HomeFooter.js
+++ frontend/src/components/HomeFooter/HomeFooter.js
@@ -1,12 +1,5 @@
 import "./styles.scss";
 
-
 export function HomeFooter(props) {
-  const { title } = props;
-
-  return (
-    <div class="footer">
-     <p>Footer</p>
-    </div>
-  );
+  return <footer class="footer" />;
 }
