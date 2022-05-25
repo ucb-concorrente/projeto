c5be0cb10f7337b94be27f26a4d87aff713e512d
Arthur <arthur.carneirontk@gmail.com>
2022-05-25 17:33:17 -0300

    Create: Frontend Setup

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,4 @@
+export function Home() {
+  return <h1>Welcome asdsadsa</h1>;
+}
+
---
+++ frontend/src/routes.js
@@ -0,0 +1,13 @@
+import { Switch, Route } from "react-router-dom";
+
+import {Home} from "./pages/Home";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+    </Switch>
+  );
+};
+
+export default Routes;
