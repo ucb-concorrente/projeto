e56091b5b8536f24c69198a52d3a539fb8206e9e
Arthur <arthur.carneirontk@gmail.com>
2022-05-25 20:27:15 -0300

    Remove: Footer

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,15 +1,10 @@
 import { HomeHeader } from "../components/HomeHeader/HomeHeader";
 import { HomeContent } from "../components/HomeContent/HomeContent";
-import { HomeFooter } from "../components/HomeFooter/HomeFooter";
 export function Home() {
   return (
     <>
       <HomeHeader title="A melhor plataforma de ingresso de MPB" />
       <HomeContent />
-      <HomeFooter />
     </>
   );
 }
-
-
-
