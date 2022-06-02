0511061e5f24b156b1ec04b4e53c0f32495c3470
Arthur <arthur.carneirontk@gmail.com>
2022-06-02 18:46:42 -0300

    Save: Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-
 echo "Limpando todas as instancias do docker, por favor, aceite."
 docker system prune -a --volumes
 
 clear
 
 echo "Instalando o maven e as dependencias."
-mvn clean install
+mvn install
 
 clear
 
 echo "Iniciando o docker."
 cd docker
 
-docker build -t ucb .
+docker run --name ucb_linux --network=host -e MYSQL_ROOT_PASSWORD=secret -e MYSQL_DATABASE=MY_DB -e MYSQL_USER=user -e MYSQL_PASSWORD=user -v :/var/lib/postgresql/data -d mysql:latest
 
-docker-compose up -d --build 
+docker build -t ucb_spring .
+docker run -d --network=host ucb_spring 
 
 echo "Database iniciado com sucesso."
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
-docker ps
+sudo docker ps
+
+echo "Instalando dependencias frontend."
+npm install yarn --global
+
+cd ../frontend && yarn && yarn start
