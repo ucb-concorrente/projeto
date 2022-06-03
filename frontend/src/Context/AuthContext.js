921d990f8e4127f153dfa56111f3c670be4d3566
Arthur <arthur.carneirontk@gmail.com>
2022-06-03 04:55:39 -0300

    Create: Authentica tion

---
+++ frontend/src/Context/AuthContext.js
@@ -0,0 +1,38 @@
+// import history from "../utils/history";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import api from "../services/api";
+const Context = React.createContext();
+
+function AuthProvider({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  function handleClick(user) {
+    const { cpf } = user;
+
+    api
+      .post(`users/auth/${cpf}`, user)
+      .then((response) => {
+        console.log(response);
+        if (response.data) {
+          setIsAuthenticated(true);
+        }
+      })
+      .catch((error) => {
+        throw new Error(error);
+      }, []);
+
+    console.log("user", user);
+
+    if (isAuthenticated) {
+      return true;
+    }
+    return false;
+  }
+  return (
+    <Context.Provider value={{ authenticated: isAuthenticated, handleClick }}>
+      {children}
+    </Context.Provider>
+  );
+}
+
+export { Context, AuthProvider };
