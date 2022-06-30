b1f6368f1558eea692de6b943cc79847b82bc50e
Mateus <mcolaresc@gmail.com>
2022-06-30 15:56:38 -0300

    edit: paths of the controllers

--- src/main/java/com/anupama/sinha/controller/EventController.java
+++ src/main/java/com/anupama/sinha/controller/EventController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/events")
+@RequestMapping(path = "/event")
 @CrossOrigin(origins = "*")
 public class EventController {
 
--- src/main/java/com/anupama/sinha/controller/TicketController.java
+++ src/main/java/com/anupama/sinha/controller/TicketController.java
@@ -4,15 +4,21 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.anupama.sinha.model.Ticket;
 import com.anupama.sinha.repository.TicketRepository;
 
+@RestController
+@RequestMapping(path = "/ticket")
+@CrossOrigin(origins = "*")
 public class TicketController {
 
   @Autowired
