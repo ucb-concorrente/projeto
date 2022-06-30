1e7291f710ff70c2f9876826934d8a75d5753268
Mateus <mcolaresc@gmail.com>
2022-06-30 01:17:11 -0300

    create: Profile repository and profile_user repository

---
+++ src/main/java/com/anupama/sinha/repository/Profile_userRepository.java
@@ -0,0 +1,9 @@
+package com.anupama.sinha.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.anupama.sinha.model.Profile_user;
+
+public interface Profile_userRepository extends JpaRepository<Profile_user, Long> {
+    
+}
